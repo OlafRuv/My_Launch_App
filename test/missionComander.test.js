381028333a2c74f1c00baf7f067f04e929d16d6c
Olaf <olaf.ruvag@gmail.com>
2022-05-11 12:31:37 -0500

    Creamis prueba de unidad para la creacion de objeto con constructor

--- test/missionComander.test.js
+++ test/missionComander.test.js
@@ -1,6 +1,8 @@
-describe('Esto es una suire de pruebas', () => {
-    test('Caso de prueba 1', () => {
-        const result = 10 * 3
-        expect(result).toBe(30);
+const MissionCommander = require('./../app/missionCommander');
+
+describe('Unit Test for Mission Commander Class', () => {
+    test('1) Create a mission commander object', () => {
+        const myMissionCommander = new MissionCommander("Woopa")
+        expect(myMissionCommander.name).toBe("Woopa")
     });
 })
